04d0e7a1ea324bb8e23e0082508358e7253da452
Balbaal <iqbal.mi20@gmail.com>
2020-09-25 00:35:35 +0800

    style: adjust element header wrapping

--- src/components/parts/Header.js
+++ src/components/parts/Header.js
@@ -3,19 +3,21 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="header__logo">Kanban.</div>
-      <ul className="header__menu-list">
-        <li className="header__menu-list__item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="header__menu-list__item">
-          <Link to="/projects">Profile</Link>
-        </li>
-        <li className="header__menu-list__item">
-          <Link to="/project/22">Logout</Link>
-        </li>
-      </ul>
+    <header className="bg-purple">
+      <div className="header container-fluid">
+        <div className="header__logo">Kanban.</div>
+        <ul className="header__menu-list">
+          <li className="header__menu-list__item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="header__menu-list__item">
+            <Link to="/projects">Profile</Link>
+          </li>
+          <li className="header__menu-list__item">
+            <Link to="/project/22">Logout</Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
