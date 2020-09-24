447c99f1614465c9a8fea2e2a1d045f0879e36e0
Balbaal <iqbal.mi20@gmail.com>
2020-09-25 01:31:39 +0800

    tweak: color purple var

--- src/components/parts/Tasks/index.js
+++ src/components/parts/Tasks/index.js
@@ -4,6 +4,9 @@ import RenderItem from "./RenderItem";
 const Tasks = () => {
   return (
     <div className="task-parent">
+      <button className="task-parent__button btn btn-md btn-primary w-100 mb-4">
+        + New Task
+      </button>
       <RenderItem />
     </div>
   );
