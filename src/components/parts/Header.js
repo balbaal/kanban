ae4326089642a092ebae7d64b2142eeae9ac8ba7
Balbaal <iqbal.mi20@gmail.com>
2020-09-24 16:30:12 +0800

    add: color variable

--- src/components/parts/Header.js
+++ src/components/parts/Header.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="header">
-      <div className="header__logo">Kanban</div>
+      <div className="header__logo">Kanban.</div>
       <ul className="header__menu-list">
         <li className="header__menu-list__item">
           <Link to="/">Home</Link>
