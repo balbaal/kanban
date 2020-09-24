a5a199fcab88c17276d2912dda1180d7b3b28487
Balbaal <iqbal.mi20@gmail.com>
2020-09-24 16:07:25 +0800

    feat: header component finished

--- src/components/parts/Header.js
+++ src/components/parts/Header.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header Component</h1>
-    </div>
+    <header className="container header">
+      <div className="header__logo">Kanban</div>
+      <ul className="header__menu-list">
+        <li className="header__menu-list__item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="header__menu-list__item">
+          <Link to="/projects">Profile</Link>
+        </li>
+        <li className="header__menu-list__item">
+          <Link to="/project/22">Logout</Link>
+        </li>
+      </ul>
+    </header>
   );
 };
 
--- src/pages/ProjectDetail.js
+++ src/pages/ProjectDetail.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+// Components
+import { Header } from "components/parts";
+
 class ProjectDetail extends React.Component {
   render() {
     console.log("this.props.match :>> ", this.props.match);
 
-    return <div>Project Detail Page: {this.props.match.params.id}</div>;
+    return (
+      <div>
+        <Header />
+        Project Detail Page: {this.props.match.params.id}
+      </div>
+    );
   }
 }
 
