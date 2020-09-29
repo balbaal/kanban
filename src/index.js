635491701f491c1d4de54daa5e944c6e2be43fb8
Balbaal <iqbal.mi20@gmail.com>
2020-09-29 14:06:23 +0800

    fix: cannot find module routing on production build

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js'
-import './assets/styles/custom.sass';
-import * as serviceWorker from './serviceWorker';
-import Routing from 'configs/routing';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import "./assets/styles/custom.sass";
+import * as serviceWorker from "./serviceWorker";
+import Routing from "./configs/routing";
 
 ReactDOM.render(
   <React.StrictMode>
     <Routing />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
